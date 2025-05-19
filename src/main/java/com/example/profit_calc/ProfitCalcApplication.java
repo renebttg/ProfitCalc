63b31cac2610e78d36e15dde12bda6b8663aef9b
renebttg <renebattaglia@hotmail.com>
2025-05-19 16:41:10 -0300

    Initial commit

---
+++ src/main/java/com/example/profit_calc/ProfitCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.profit_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/profit_calc/ProfitCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profit_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
